5931691c4f02f75cba797a6aa5d8ccbfba004da0
Jennifer Burns <jburns@mitre.org>
2017-10-12 09:01:42 -0400

    Updated ext refs to handle citation name changes properly

--- src/app/components/external-reference/external-reference.component.ts
+++ src/app/components/external-reference/external-reference.component.ts
@@ -15,6 +15,7 @@ export class ExternalReferenceComponent {
 
     public addExternalReferenceButtonClicked(): void {
         let externalReference = new ExternalReference();
+        externalReference.citeButton = "Generate Citation Text";
         this.model.attributes.external_references.unshift(externalReference);
     }
 
@@ -22,11 +23,13 @@ export class ExternalReferenceComponent {
         this.model.attributes.external_references = this.model.attributes.external_references.filter((h) => h !== externalReference);
     }
 
-    public generateCitation(citationStr: string): void {
-        let match = this.model.attributes.external_references.find((h) => h.source_name === citationStr);
-        let index = this.model.attributes.external_references.indexOf(match);
-        this.model.attributes.external_references[index].citation = "[[Citation: " + citationStr + "]]";
-        this.model.attributes.external_references[index].citeref = "[[CiteRef::" + citationStr + "]]";
+    public generateCitation(extRef: string): void {
+        if(extRef.citeButton == "Generate Citation Text"){
+            extRef.citeButton = "Hide Citation Text";
+        }
+        else{
+            extRef.citeButton = "Generate Citation Text";
+        }
     }
 
     public populateCitation(citationStr: string): void {
@@ -38,6 +41,13 @@ export class ExternalReferenceComponent {
             this.model.attributes.external_references[index].description = citation.description;
             this.model.attributes.external_references[index].url = citation.url;
         }
+        else{
+            this.model.attributes.external_references[index].external_id = '';
+            this.model.attributes.external_references[index].description = '';
+            this.model.attributes.external_references[index].url = '';
+        }
+        this.model.attributes.external_references[index].citation = "[[Citation: " + citationStr + "]]";
+        this.model.attributes.external_references[index].citeref = "[[CiteRef::" + citationStr + "]]";
         if(citationStr){
             let filterVal = citationStr.toLowerCase();
             return this.citations.filter((h) => h.source_name.toLowerCase().startsWith(filterVal));

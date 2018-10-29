bd4e644925b2dfc1203397f7ab2d501f2761d1c8
Jennifer Burns <jburns@mitre.org>
2018-10-29 14:26:21 -0400

    Removed wiki reference

--- src/app/settings/stix-objects/attack-patterns/attack-pattern-pre-new/attack-pattern-pre-new.component.ts
+++ src/app/settings/stix-objects/attack-patterns/attack-pattern-pre-new/attack-pattern-pre-new.component.ts
@@ -64,7 +64,7 @@ export class AttackPatternPreNewComponent extends AttackPatternEditComponent imp
             this.mitreId = new ExternalReference();
             this.mitreId.external_id = this.id;
             this.mitreId.source_name = 'mitre-pre-attack';
-            this.mitreId.url = 'https://attack.mitre.org/wiki/Technique/' + this.id
+            this.mitreId.url = 'https://attack.mitre.org/techniques/' + this.id
         } else {
             this.mitreId = new ExternalReference();
             this.mitreId.source_name = 'mitre-pre-attack';
